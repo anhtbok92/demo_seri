aef564cbe5809961d6590e51f2db5d1cb2e443c0
anhtbok92 <victory1080@gmail.com>
2022-03-19 14:29:43 +0700

    fix

--- 6-mongosee-and-mongodb/layoutMongo/app.js
+++ 6-mongosee-and-mongodb/layoutMongo/app.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// for upload image
 var fileupload = require('express-fileupload');
 app.use(fileupload());
 
